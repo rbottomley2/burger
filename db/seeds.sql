1d3e572b214610409948e1da81d97d0c83225375
Robert Bottomley <rbottomley2@att.net>
2017-11-11 04:28:26 -0800

    new commit

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO `burgers_db`.`burgers`
+  (`burger_name`, `devoured`)
+  VALUES ('Big Mac', 1);
+
+INSERT INTO `burgers_db`.`burgers`
+  (`burger_name`, `devoured`)
+  VALUES ('Whopper', 0);
+
+INSERT INTO `burgers_db`.`burgers`
+  (`burger_name`, `devoured`)
+  VALUES ('Double Double', 0);
